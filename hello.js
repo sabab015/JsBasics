0a5c73e234f0c27fc0414fb47c2f0a170c2f58e3
AL SABAB <sabab015@gmail.com>
2022-11-01 14:27:35 +0600

    variables

--- hello.js
+++ hello.js
@@ -1,5 +1,11 @@
-console.log(21);
-console.log("Hello World");
+// console.log(21);
+// console.log("Hello World");
 
-var price = 1.2;
-console.log(price);
+// var price = 1.2;
+// console.log(price);
+
+var seenAfter = '12';
+console.log(typeof seenAfter);
+
+var isTrue = true;
+console.log(typeof isTrue);
