799f28f8f36e0f9173a2edf2f455fdc2360e2c8e
AL SABAB <sabab015@gmail.com>
2022-11-01 17:04:53 +0600

    variable parsing

--- hello.js
+++ hello.js
@@ -4,8 +4,16 @@
 // var price = 1.2;
 // console.log(price);
 
-var seenAfter = '12';
-console.log(typeof seenAfter);
+// var seenAfter = '12';
+// console.log(typeof seenAfter);
 
-var isTrue = true;
-console.log(typeof isTrue);
+// var isTrue = true;
+// console.log(typeof isTrue);
+
+// var promise = "Javascript for Beginners";
+// console.log(promise.toLowerCase()); 
+
+var number1 = 10;
+var number2 = '20.5';
+number2 = parseFloat(number2);
+console.log(number1+number2);
