62d623f5ccdb29254c8e4ead8e8c8c071b36eeb0
AL SABAB <sabab015@gmail.com>
2022-11-02 12:28:29 +0600

    array added

---
+++ array.js
@@ -0,0 +1,4 @@
+var age = [10,15,16,85];
+age [3] = 18;
+console.log(age.indexOf(16));
+console.log(age);
--- hello.js
+++ hello.js
@@ -1,36 +1,36 @@
-// console.log(21);
-// console.log("Hello World");
+console.log(21);
+console.log("Hello World");
 
-// var price = 1.2;
-// console.log(price);
+var price = 1.2;
+console.log(price);
 
-// var seenAfter = '12';
-// console.log(typeof seenAfter);
+var seenAfter = '12';
+console.log(typeof seenAfter);
 
-// var isTrue = true;
-// console.log(typeof isTrue);
+var isTrue = true;
+console.log(typeof isTrue);
 
-// var promise = "Javascript for Beginners";
-// console.log(promise.toLowerCase()); 
+var promise = "Javascript for Beginners";
+console.log(promise.toLowerCase()); 
 
-// var number1 = 10;
-// var number2 = '20.5';
-// number2 = parseFloat(number2);
-// console.log(number1+number2);
+var number1 = 10;
+var number2 = '20.5';
+number2 = parseFloat(number2);
+console.log(number1+number2);
 
-// var number1 = 0.1;
-// var number2 = 0.2;
-// var total = number1 + number2;
-// total = total.toFixed(2);
-// console.log(total);
+var number1 = 0.1;
+var number2 = 0.2;
+var total = number1 + number2;
+total = total.toFixed(2);
+console.log(total);
 
-// var name1 = 'al';
-// var name2 = 'sabab';
-// console.log(name1+ ' ' +name2);
+var name1 = 'al';
+var name2 = 'sabab';
+console.log(name1+ ' ' +name2);
 
-// var num = -5;
-// num = Math.abs(num);
-// console.log(num);
+var num = -5;
+num = Math.abs(num);
+console.log(num);
 
 var date = new Date('1971-12-16');
 console.log(date);
