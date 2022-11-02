a496266ea6f6dcef0384e6ba89c71246827c7308
AL SABAB <sabab015@gmail.com>
2022-11-02 13:58:49 +0600

    array updated

--- array.js
+++ array.js
@@ -1,4 +1,6 @@
-var age = [10,15,16,85];
-age [3] = 18;
+var age = [10, 15, 16, 85];
+age[3] = 18;
 console.log(age.indexOf(16));
+console.log(age);
+age.push(20);
 console.log(age);
--- conditionals.js
+++ conditionals.js
@@ -1,9 +1,9 @@
 var price = 08;
 
-if(price<10){
+if (price < 10) {
     console.log("i'll buy");
-}else if(price == 15){
+} else if (price == 15) {
     console.log("i'll think about it");
-}else{
+} else {
     console.log("i won't");
 }
