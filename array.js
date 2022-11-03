42c5a40b531d848fc3655dc9b435c75607f4b741
AL SABAB <sabab015@gmail.com>
2022-11-03 12:38:33 +0600

    loops

--- array.js
+++ array.js
@@ -1,6 +1,15 @@
 var age = [10, 15, 16, 85];
+
 age[3] = 18;
+
 console.log(age.indexOf(16));
+
 console.log(age);
+
 age.push(20);
+
 console.log(age);
+
+age.pop(20);
+
+console.log(age.shift());
---
+++ loop.js
@@ -0,0 +1,15 @@
+var num = 0;
+while(num <15){
+    console.log(num);
+    num++;
+}
+
+for(i=0;i<15;i++){
+    console.log(i);
+}
+
+var nums = [10,12,14,16,18,20,22];
+for(i=0;i<nums.length;i++){
+    var element = nums[i];
+    console.log(element);
+}
