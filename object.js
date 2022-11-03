46312ac9b07e9fd49bcb52c863f4f56155d98c3c
AL SABAB <sabab015@gmail.com>
2022-11-03 16:43:50 +0600

    objects added

---
+++ object.js
@@ -0,0 +1,15 @@
+var student1 = {id:172, name:"sakib", phone: 172};
+var student2 = {id:162, name:"sadat", phone: 167};
+
+//value before update
+console.log(student1);
+console.log(student2);
+
+//update values
+student2.phone = 567;
+student1.name = "sabab"; 
+student1.location = "dhaka"
+
+//value after update
+console.log(student1);
+console.log(student2);
