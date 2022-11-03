f3e4594c32fa59a0a929be9e0126b42ad3faa6ad
AL SABAB <sabab015@gmail.com>
2022-11-03 14:34:19 +0600

    functions

---
+++ function.js
@@ -0,0 +1,12 @@
+function double(num){
+    var result = num * 2;
+    console.log(result);
+}
+double(5);
+double(0.75);
+
+function add(num1, num2){
+    var result = num1 + num2;
+    console.log(result);
+}
+add(15,15);
---
+++ switch.js
@@ -0,0 +1,12 @@
+var num = 5;
+switch (num) {
+  case 10:
+    console.log("its 10");
+    break;
+  case 5:
+    console.log("its 5");
+    break;
+  default:
+    console.log("its nothing");
+    break;
+}
