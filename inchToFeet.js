20ebab392d0f6477001ca3ad89c3a3930c9936fb
AL SABAB <sabab015@gmail.com>
2022-11-05 11:13:07 +0600

    basic js

---
+++ inchToFeet.js
@@ -0,0 +1,9 @@
+function inchToFeet(inch){
+    var feet = inch/12;
+    return feet;
+}
+var num0 = [156,288]
+var num1 = inchToFeet(36);
+var num00 = inchToFeet(num0[0]);
+console.log(num1);
+console.log(num00);
---
+++ leapYear.js
@@ -0,0 +1,8 @@
+const year = 2000;
+let remainder = year % 4;
+
+if (remainder == 0) {
+  console.log("Leap Year");
+} else {
+  console.log("Not Leap Year");
+}
